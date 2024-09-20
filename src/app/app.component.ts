77ea512c3d2b59b487f05556dd702f7b3165a293
Ramza <Ramza@gmail.com>
2024-09-20 18:20:18 +0300

    new adds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgFor, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+	headerItems = ['Главная', 'Компания']
+
+	isItemHide = true
 }
