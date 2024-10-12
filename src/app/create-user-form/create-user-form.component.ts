de70aa04d5086264981beb40105f18b3191c9d6c
Khao Kai <Ramza@gmail.com>
2024-10-12 20:16:59 +0300

    create-user is ready

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -1,13 +1,45 @@
 import { NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+	NgForm,
+	ReactiveFormsModule,
+	AbstractControl,
+	FormControl,
+	FormGroup,
+	FormGroupDirective,
+	FormsModule,
+	Validators
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+	isErrorState(control: AbstractControl | null, form: FormGroupDirective | NgForm | null): boolean {
+		const isSubmitted = form && form.submitted;
+		return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+	};
+}
 
 @Component({
-  selector: 'app-create-user-form',
-  standalone: true,
-  imports: [ReactiveFormsModule, NgIf],
-  templateUrl: './create-user-form.component.html',
-  styleUrl: './create-user-form.component.scss'
+	selector: 'app-create-user-form',
+	standalone: true,
+	imports: [
+		ReactiveFormsModule,
+		NgIf,
+		FormsModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatDividerModule,
+		MatInputModule,
+		MatButtonModule,
+		MatIconModule,
+	],
+	templateUrl: './create-user-form.component.html',
+	styleUrl: './create-user-form.component.scss',
 })
 export class CreateUserFormComponent {
 	@Output()
@@ -20,8 +52,10 @@ export class CreateUserFormComponent {
 		companyName: new FormControl('', [Validators.required, Validators.minLength(2)]),
 	});
 
+	public readonly matcher = new MyErrorStateMatcher();
+
 	public submitForm(): void {
 		this.createUser.emit(this.form.value);
 		this.form.reset();
-	}
+	};
 }
