53171d369dd92dce8051330288443f2f42d58ebd
Khao Kai <Ramza@gmail.com>
2024-11-03 19:40:02 +0300

    add Ilnur hard feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,82 @@
-import { DatePipe, NgFor, NgIf } from '@angular/common';
-import { Component, inject } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { AsyncPipe, DatePipe, NgFor, NgIf } from '@angular/common';
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import { BackgroundColorChangeDirective } from './directives/background-color-change.directive';
 import { UserService } from './services/user.service';
+import { AdminOrUserComponent } from './admin-page/admin-or-user/admin-or-user.component';
+import { MatDialog } from '@angular/material/dialog';
+import { map } from 'rxjs';
+import { IUserOrAdmin } from './interfaces/user-admin.interface';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    RouterOutlet,
-    RouterLink,
-    NgFor,
-    NgIf,
-    DatePipe,
-    BackgroundColorChangeDirective,
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+	selector: 'app-root',
+	standalone: true,
+	imports: [
+		RouterOutlet,
+		RouterLink,
+		NgFor,
+		NgIf,
+		DatePipe,
+		BackgroundColorChangeDirective,
+		AsyncPipe,
+	],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.scss',
+	changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AppComponent {
-  public readonly userService = inject(UserService);
-  public readonly title = 'mentoring-first-project';
-  public headerItems = ['Главная', 'Компания'];
-  public isAboutUs = true;
-  public today: number = Date.now();
+	public readonly userService = inject(UserService);
+	public readonly title = 'mentoring-first-project';
+	public headerItems = ['Главная', 'Компания'];
+	public isAboutUs = true;
+	public today: number = Date.now();
+	private readonly dialog = inject(MatDialog);
+
+	private readonly router = inject(Router)
+	public status: string = '';
+	public isShowAdminBtn: boolean = false;
+
+	public userRole$ = this.userService.userOrAdmin$.pipe(
+		map((data: IUserOrAdmin | { user: null }) => {
+			// for (let key in data) {
+			// 	if (key === 'user') {
+			// 		this.status = 'Войти';
+			// 		return true;
+			// 	}
+			// }
+			// this.status = 'Выйти';
+			// return false;
+
+			// или:
+
+			if ('user' in data) {
+				this.status = 'Войти';
+				this.isShowAdminBtn = false;
+				return true;
+			} else {
+				this.status = 'Выйти';
+				this.isShowAdminBtn = true;
+				return false;
+			}
+		})
+	);
+
+	public openDialog(): void {
+		if (this.status === 'Войти') {
+			this.dialog
+				.open(AdminOrUserComponent, {
+					width: '600px',
+				})
+				.afterClosed()
+				.subscribe((result: any) => {
+					if (result === true) {
+						this.userService.loginAsAdmin();
+					} else if (result === false) {
+						this.userService.loginAsUser();
+					}
+				});
+		} else if (this.status === 'Выйти') {
+			confirm('Вы точно хотите выйти?') ? (this.userService.logout(), this.router.navigate([''])) : null
+		}
+	}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,8 @@ import { MainContentComponent } from './main-content/main-content.component';
 import { UsersListComponent } from './users-list/users-list.component';
 import { TodosListComponent } from './todos-list/todos-list.component';
 import { AdminPageComponent } from './admin-page/admin-page.component';
-import { checkAdminGuard } from './guards/срñcheck-admin.guard';
 import { exitGuard } from './guards/exit.guard';
+import { checkAdminGuard } from './guards/check-admin.guard';
 
 export const routes: Routes = [
   {
